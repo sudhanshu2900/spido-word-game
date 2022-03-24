0a86261e3384559f6d4c4e74616115589dfa9f98
Sudhanshu Gupta <52392822+sudhanshu2900@users.noreply.github.com>
2022-03-24 12:42:25 +0530

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footerDiv'>
+        <p>Designed and developed 💻 by <strong>Sudhanshu Gupta</strong> with 💖</p>
+    </div>
+  )
+}
+
+export default Footer
